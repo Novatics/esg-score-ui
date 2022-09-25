7a7cf9c94cc8e276906bfb66360ffd90880fa8b3
vitor <vitorribeiro221196@gmail.com>
2022-09-25 17:16:33 -0300

    fix: remove amplify setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,7 @@
-import { Amplify } from 'aws-amplify'
 import ReactDOM from 'react-dom'
 import 'global.d.ts'
 import App from './App'
 import 'reset-css'
 import 'common/css/fonts.css'
-
-Amplify.configure({
-  Auth: {
-    region: process.env.REACT_APP_COGNITO_REGION,
-    userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID,
-  },
-})
 
 ReactDOM.render(<App />, document.getElementById('root'))
